b5abe4647fa4863a926357c95b942ba647ac620d
Marcelo Manzan <manzan@gmail.com>
2015-11-25 13:59:12 -0200

    Create account_profiles.mssql.table.sql

---
+++ db/tables/account_profiles.mssql.table.sql
@@ -0,0 +1,28 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [ledger].[account_profiles](
+  [id] int IDENTITY(1,1) NOT NULL,
+  [issuer_id] int NOT NULL,
+  [identifier] varchar(20) NOT NULL,
+  [name] varchar(50) NOT NULL,
+  CONSTRAINT [PK_account_profiles] PRIMARY KEY CLUSTERED 
+(
+  [id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY],
+ CONSTRAINT [UK_account_profiles_identifier] UNIQUE NONCLUSTERED 
+(
+  [identifier] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [ledger].[account_profiles]  WITH CHECK ADD  CONSTRAINT [FK_account_profiles__issuers] FOREIGN KEY([issuer_id])
+REFERENCES [ledger].[issuers] ([id])
+ON DELETE CASCADE
+
+SET ANSI_PADDING OFF
+GO
