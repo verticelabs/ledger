8f6038f1b968203870e715302e0dcba1634c73e4
Marcelo Manzan <manzan@gmail.com>
2015-12-10 00:53:23 -0200

    refactoring to keep only diagrams and specs

--- ledger-core/src/main/java/verticelabs/ledger/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package verticelabs.ledger.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import verticelabs.ledger.domain.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
